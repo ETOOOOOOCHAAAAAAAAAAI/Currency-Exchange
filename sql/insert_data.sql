fa4871b48ec405675eb81f37dca7cf0e1615c328
LUBOVO PARVU <makstarsenkov@gmail.com>
2026-06-03 21:06:17 +0500

    fix : adding new lib and fix db settings

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -2,6 +2,6 @@ INSERT INTO Currencies (code, full_name, sign) VALUES ('USD', 'United States dol
 INSERT INTO Currencies (code, full_name, sign) VALUES ('EUR', 'Euro', '€');
 INSERT INTO Currencies (code, full_name, sign) VALUES ('RUB', 'Russian Ruble', '₽');
 
-INSERT INTO ExchangeRates (base_currency_id, target_currency_id, rate) VALUES (1, 2, 0.95);
-INSERT INTO ExchangeRates (base_currency_id, target_currency_id, rate) VALUES (1, 3, 90.50);
-INSERT INTO ExchangeRates (base_currency_id, target_currency_id, rate) VALUES (2, 3, 95.00);
+INSERT INTO Exchange_Rates (base_currency_id, target_currency_id, rate) VALUES (1, 2, 0.95);
+INSERT INTO Exchange_Rates (base_currency_id, target_currency_id, rate) VALUES (1, 3, 90.50);
+INSERT INTO Exchange_Rates (base_currency_id, target_currency_id, rate) VALUES (2, 3, 95.00);
